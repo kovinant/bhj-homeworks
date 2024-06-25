0041410427f394bf5801b71fb807ae2f24c6a4da
Akytagava <kabysh_nik@mail.ru>
2024-06-25 23:25:46 +0300

    Изменения

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var timerElement = document.getElementById('timer');
-    var timeLeft = parseInt(timerElement.textContent, 10);
+document.addEventListener("DOMContentLoaded", () => {
+    const cookie = document.getElementById('cookie');
+    const counter = document.getElementById('clicker__counter');
+    let clickCount = 0;
+    let isCookieBig = false;
+    
+    cookie.addEventListener('click', () => {
 
-    var countdown = setInterval(function() {
-        if (timeLeft > 0) {
-            timeLeft -= 1;
-            timerElement.textContent = timeLeft;
+        clickCount++;
+        counter.textContent = clickCount;
+
+ 
+        if (isCookieBig) {
+            cookie.style.width = '200px';
+            cookie.style.height = 'auto';
         } else {
-            clearInterval(countdown);
-            alert('Вы победили в конкурсе!');
+            cookie.style.width = '220px';
+            cookie.style.height = 'auto';
         }
-    }, 1000);
+        isCookieBig = !isCookieBig;
+    });
 });
